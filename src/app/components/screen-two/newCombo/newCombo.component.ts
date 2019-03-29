b3a4a782b34b09c80e99c98e63a9334b55c2d1f3
Juan Aguillon <miloca.98@hotmail.com>
2019-03-29 18:28:04 -0500

    Pestañas Agregadas

--- src/app/components/screen-two/newCombo/newCombo.component.ts
+++ src/app/components/screen-two/newCombo/newCombo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from 'src/app/services/database.service';
 
 @Component({
   selector: 'app-newCombo',
@@ -7,9 +8,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewComboComponent implements OnInit {
 
-  constructor() { }
+  vals:any = {}
+  inValues:any = {}
+  theCombo:any[] = []
+  statusVals = [
+    "Guardar",
+    "<span class='spinner-border spinner-border-sm' role='status' aria-hidden='true'></span>",
+    "Guardado"
+  ]
+  status = 0;
+  
+  constructor(
+    private database:DatabaseService
+  ) { }
 
-  ngOnInit() {
+  ngOnInit() {}
+  
+  addCombo( ){
+    let lengt = Object.keys( this.vals ).length;
+    if ( lengt < 2 ){
+      alert('Añade nombre y precio a el combo')
+    }
+
+    this.theCombo.push(this.vals )
+    this.vals = {};
   }
+
+  enteringCombo( e ){
+    e.preventDefault();
+    if ( e.keyCode == 13 ){
+      this.addCombo ( );
+    }
+  }
+
+  saveDocument( ){
+    this.inValues.id = new Date().getTime();
+    this.inValues.type = "combo";
+    this.inValues.combos = this.theCombo;
+    this.status = 1;
+    const onProm = res => {
+      alert('Combos guardados correctamente');
+      this.status = 2;
+    }
+
+    const onError = error => {
+      alert('Error al crear el combo')
+      console.log( error );
+    }
+    
+    
+    let saved = this.database.saveDocument( 'secondscreen', this.inValues );
+    saved.then( onProm );
+    saved.catch ( err => console.log( err ) );
+    
+  }
+
 
 }
--- src/app/components/screen-two/newEjecutive/newEjecutive.component.ts
+++ src/app/components/screen-two/newEjecutive/newEjecutive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from 'src/app/services/database.service';
 
 @Component({
   selector: 'app-newEjecutive',
@@ -7,9 +8,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewEjecutiveComponent implements OnInit {
 
-  constructor() { }
+  vals: any = {}
+  currentAdition = ""
+  aditions = [];
+  menus = []
 
-  ngOnInit() {
+  inValues: any = {}
+  statusVals = [
+    "Guardar",
+    "<span class='spinner-border spinner-border-sm' role='status' aria-hidden='true'></span>",
+    "Guardado"
+  ]
+  status = 0;
+
+  constructor(
+    private database: DatabaseService
+  ) { }
+
+  ngOnInit() { }
+
+  addAdition() {
+    
+    if ( this.currentAdition == "") return false;
+    
+    this.aditions.push(this.currentAdition)
+    this.currentAdition = "";
+  }
+
+  enteringAdition(e) {
+    e.preventDefault();
+    if (e.keyCode == 13) {
+      this.addAdition();
+    }
+  }
+
+
+  addMenu( ){
+    let data = {
+      name: this.vals.title,
+      price: this.vals.price,
+      aditions: this.aditions
+    }
+
+    this.menus.push( data );
+
+    this.aditions = [ ]
+    this.vals = {}
+    
+  }
+
+  saveDocument() {
+    this.inValues.id = new Date().getTime();
+    this.inValues.type = "ejecutive";
+    this.inValues.menus = this.menus;
+    
+    this.status = 1;
+
+    const onProm = res => {
+      alert('Menu ejecutivo guardado correctamente');
+      this.status = 2;
+    }
+
+    const onError = error => {
+      alert('Error al crear el combo')
+      console.log(error);
+    }
+
+
+    let saved = this.database.saveDocument('secondscreen', this.inValues);
+    saved.then(onProm);
+    saved.catch(err => console.log(err));
+
   }
 
 }
