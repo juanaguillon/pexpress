c6eaa03f7f8c789e23b6d6f9315206e65f036537
Juan Aguillon <miloca.98@hotmail.com>
2019-05-06 12:08:19 -0500

    NewFds Modificado

--- src/app/components/screen-two/newfds/newfds.component.ts
+++ src/app/components/screen-two/newfds/newfds.component.ts
@@ -17,6 +17,8 @@ export class NewFdsComponent implements OnInit {
   launchers = [];
   launcData:any = {}
 
+
+  buttonSend:any;
   currentID:number;
 
   // Valores que serán almacenadas en la base de datos.
@@ -24,6 +26,9 @@ export class NewFdsComponent implements OnInit {
 
   
   ngOnInit() { 
+
+    this.setTypeButtonSend('add')
+    
     this.subscribeData = this.searchInfo.subscribe( doc => {
 
       this.inValues.id = this.currentID != null ? this.currentID : new Date().getTime();
@@ -56,12 +61,75 @@ export class NewFdsComponent implements OnInit {
   }
 
   addLaunchKey( eeve ){
-    if (eeve.keyCode == 13 ){
-      this.addLaunch(  )
+    if (eeve.keyCode == 13  ){
+
+      if (this.buttonSend.action == "add"){
+        this.addLaunch(  )
+
+      } else if (this.buttonSend.action == "edit" ){
+        this.setEditAdition(this.buttonSend.index );
+        this.setTypeButtonSend('add');
+      }
     }
   }
   
   removeAdition( i ){
     this.launchers.splice( i,1 );
+  }
+
+
+  prepareToEditAdition(e) {
+    e.preventDefault()
+
+    this.setTypeButtonSend('edit');
+    this.buttonSend.index = e.target.getAttribute('data-index')
+    let i = this.buttonSend.index;
+
+    this.launcData = {
+      name: this.launchers[i].name,
+      price: this.launchers[i].price
+    }
+  }
+
+  /* Guardar menú modificado */
+  private setEditAdition(i) {
+    let newVal = {
+      name: this.launcData.name,
+      price: this.launcData.price
+    }
+    this.launchers[i] = newVal;
+    this.launcData = {}
+  }
+
+  /** Se ejecuta al clickear el botón para agregar/editar menú */
+  sendEditingAdition(e) {
+    e.preventDefault();
+    let event = e.target.getAttribute('data-event');
+
+    if (event == 'add') {
+      this.addLaunch();
+    } else if (event == 'edit') {
+
+      let index = e.target.getAttribute('data-index');
+      this.setEditAdition(index);
+      this.setTypeButtonSend("add")
+    }
+  }
+
+  /** Cambiar el botón de añadir/editar menú */
+  private setTypeButtonSend(type: string) {
+    if ('add' == type) {
+      this.buttonSend = {
+        text: "Agregar almuerzo",
+        className: 'btn btn-success btn-sm my-1',
+        action: 'add'
+      }
+    } else if (type == 'edit') {
+      this.buttonSend = {
+        text: "Editar almuerzo",
+        className: 'btn btn-primary btn-sm my-1',
+        action: 'edit'
+      }
+    }
   }
 }
